0b8538cf7aae81de158802b16a249574a02b6a4a
stamplevskiyd <stamplevskiyd@gmail.com>
2022-04-11 21:57:50 +0300

    3rd lesson

--- project/app/blogengine/blog/views.py
+++ project/app/blogengine/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def posts_list(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    n = ['Oleg', 'Masha', 'Watson']
+    return render(request, 'blog/index.html', context={'names': n})
